fedf4336e120b4a9795b2920ad12d64956fc052d
udinaanna850-dotcom <udinaanna850@gmail.com>
2026-09-18 16:36:57 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
+        int t = int.Parse(Console.ReadLine());
+
+        m = m + t;
+
+        n = n + m / 60;
+        m = m % 60;
+
+        n = n % 24;
+
+        Console.WriteLine(n + ":" + m);
+    }
+}
