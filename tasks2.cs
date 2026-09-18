ec15a095c5768d3fbb131f09a7ec1ea5f37c282f
udinaanna850-dotcom <udinaanna850@gmail.com>
2026-09-18 16:41:13 +0500

    Add files via upload

---
+++ tasks2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        string result = "";
+
+        while (a > 0)
+        {
+            result = (a % 2) + result;
+            a = a / 2;
+        }
+
+        Console.WriteLine(result);
+    }
+}
